f6bb86da41d4b667e47933021a7604cd68d464b2
KDragon <elkarouani@gmail.com>
2019-01-09 00:26:17 +0100

    Task: adding random choices getter function

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+function getComputerChoice() {
+	const choices = ['r', 'p', 's'];
+	const randomNumber = Math.floor(Math.random() * 3);
+	return choices[randomNumber];
+}
+
+function game(userChoice) {
+	console.log(userChoice);
+}
+
 function main() {
 	rock_div.addEventListener('click', function(){
 		game("r");

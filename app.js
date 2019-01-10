7f0ba7b2adedd507df4fbd1f7a193e55d52e2b47
KDragon <elkarouani@gmail.com>
2019-01-10 12:08:01 +0100

    Task: adding convertToWord function

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -14,8 +14,55 @@ function getComputerChoice() {
 	return choices[randomNumber];
 }
 
+function convertToWord(letter) {
+	switch (letter) {
+		case "r":
+			return "Rock";
+			break;
+		case "p":
+			return "Paper";
+			break;
+		case "s":
+			return "Scissors";
+			break;
+	}
+}
+
+function win(userChoice, computerChoice) {
+	userScore++;
+	userScore_span.innerHTML = userScore;
+	computerScore_span.innerHTML = computerScore;
+	result_p.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(computerChoice) + ". You win!";
+}
+
+function lose(userChoice, computerChoice) {
+	
+}
+
+function draw(userChoice, computerChoice) {
+	
+}
+
 function game(userChoice) {
-	console.log(userChoice);
+	const computerChoice = getComputerChoice();
+
+	switch (userChoice + computerChoice) {
+		case "rs":
+		case "sp":
+		case "pr":
+			win(userChoice, computerChoice);
+			break;
+		case "rp":
+		case "ps":
+		case "sr":
+			lose(userChoice, computerChoice);
+			break;
+		case "rr":
+		case "pp":
+		case "ss":
+			draw(userChoice, computerChoice);
+			break;
+	}
 }
 
 function main() {
